2af11e11b0d1d00a4bcfddc6c35c42ecee1503a7
Gary <hai.alphacreative@gmail.com>
2025-02-25 17:51:55 +0700

    a

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ function handlePageVisibilityAndFavicon() {
     isBlinking = true;
     const favicons = [
       "./assets/images/utilize/favicon_red.svg",
-      "./assets/images/utilize/favicon_black.svg"
+      "./assets/images/utilize/favicon_black.svg",
     ];
     let faviconIndex = 0;
 
@@ -96,9 +96,9 @@ function loading() {
     onComplete: function () {
       gsap.to(".dots", {
         scale: 1,
-        transformOrigin: "center"
+        transformOrigin: "center",
       });
-    }
+    },
   });
 
   gsap.fromTo(".loading-overlay", { opacity: 0 }, { opacity: 1, duration: 1 });
@@ -125,7 +125,7 @@ function toggleMenu() {
     y: 20,
     stagger: 0.1,
     duration: 0.6,
-    ease: "power2.out"
+    ease: "power2.out",
   }).from(
     ".menu__social ul li",
     {
@@ -133,7 +133,7 @@ function toggleMenu() {
       y: 20,
       stagger: 0.1,
       duration: 0.6,
-      ease: "power2.out"
+      ease: "power2.out",
     },
     "-=0.4"
   );
@@ -154,8 +154,8 @@ function scrollHeader() {
       trigger: "body",
       start: "top+=100 top",
       toggleClass: { targets: ".header", className: "scrolled" }, //
-      once: false
-    }
+      once: false,
+    },
   });
 }
 
@@ -175,8 +175,8 @@ function textQuote() {
         markers: false,
         scrub: 1,
         start: "top center",
-        end: "bottom center"
-      }
+        end: "bottom center",
+      },
     });
   });
 }
@@ -185,7 +185,7 @@ function magicCursor() {
 
   gsap.set(circle, {
     xPercent: -50,
-    yPercent: -50
+    yPercent: -50,
   });
 
   let mouseX = 0,
@@ -207,7 +207,7 @@ function magicCursor() {
       x: posX,
       y: posY,
       ease: "power3.out",
-      duration: 0.3
+      duration: 0.3,
     });
 
     requestAnimationFrame(moveCircle);
@@ -281,8 +281,8 @@ function ourProjects() {
             .querySelector(".section-projects")
             .classList.remove("on-active");
         }
-      }
-    }
+      },
+    },
   });
 
   // gsap.to(".section-projects", {
@@ -307,7 +307,7 @@ function ourProjects() {
       y: 20,
       stagger: 0.1,
       duration: 0.5,
-      ease: "none"
+      ease: "none",
     },
     "-=0.3"
   );
@@ -374,8 +374,8 @@ function bannerBall() {
         } else {
           $(".hero__content").removeClass("change");
         }
-      }
-    }
+      },
+    },
   });
 }
 
@@ -454,7 +454,7 @@ function itemParalax() {
     gsap.fromTo(
       wrap,
       {
-        y: y
+        y: y,
       },
       {
         y: 0,
@@ -464,9 +464,9 @@ function itemParalax() {
           end: "bottom top",
           scrub: 1,
           ease: "power4",
-          delay: 0.2
+          delay: 0.2,
           // markers: true
-        }
+        },
       }
     );
   });
@@ -484,7 +484,7 @@ function gallery() {
     trigger: ".gallery__container ",
     start: "top top",
     end: "bottom bottom",
-    pin: ".right"
+    pin: ".right",
   });
 
   details.forEach((detail, index) => {
@@ -493,7 +493,7 @@ function gallery() {
       .timeline()
       .to(photos[index], {
         clipPath: "inset(0% 0% 0% 0%)",
-        duration: 2.5
+        duration: 2.5,
       })
       .set(allPhotos[index], { autoAlpha: 0 });
     ScrollTrigger.create({
@@ -508,7 +508,7 @@ function gallery() {
       },
       onLeaveBack: () => {
         headline.classList.remove("active");
-      }
+      },
     });
   });
 
@@ -520,7 +520,7 @@ function gallery() {
     transformOrigin: "center center",
     xPercent: -50,
     yPercent: -50,
-    y: 0
+    y: 0,
   });
 
   // Animation di chuyển vòng tròn khi cuộn
@@ -535,7 +535,7 @@ function gallery() {
       let moveY = progress * window.innerHeight;
 
       gsap.to(line, { y: moveY, duration: 0.1, ease: "none" });
-    }
+    },
   });
 }
 function scrollBall() {
@@ -547,8 +547,8 @@ function scrollBall() {
       end: "bottom top",
       scrub: 2,
       // markers: true,
-      invalidateOnRefresh: true
-    }
+      invalidateOnRefresh: true,
+    },
   });
   tl.fromTo(
     ".projects-ball",
@@ -560,9 +560,9 @@ function scrollBall() {
           { x: "50vw", y: "100vh" },
           { x: "25vw", y: "150vh" },
           { x: "5vw", y: "200vh" },
-          { x: "0vw", y: "300vh" }
-        ]
-      }
+          { x: "0vw", y: "300vh" },
+        ],
+      },
     }
   );
   // tl.to(".projects-ball", { top: "30%", left: "60%", ease: "power1.inOut" })
@@ -578,9 +578,9 @@ function sectionServices() {
       start: "-64px top",
       end: "bottom bottom",
       scrub: 1,
-      pin: ".services-wrapper__left"
+      pin: ".services-wrapper__left",
       // markers: true
-    }
+    },
   });
 
   // Animation di chuyển vòng tròn khi cuộn
@@ -602,12 +602,12 @@ function sectionServices() {
       if (progress >= 0.93) {
         document.querySelector(".section-projects").classList.add("touch");
       }
-    }
+    },
   });
 }
 function swiperLogo() {
-  if ($(".swiper-logo").length < 1) return;
-  var swiper = new Swiper(".swiper-logo", {
+  if ($(".swiper__logo").length < 1) return;
+  var swiper = new Swiper(".swiper__logo", {
     slidesPerView: 6,
     loop: true,
     centeredSlides: true,
